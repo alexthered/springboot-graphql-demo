acb0514fbcc493a580cba7e058891a6fec7ad492
Hiep Doan <hiep.dv179@gmail.com>
2020-07-06 21:53:06 +0200

    🎉 Initial commit

---
+++ src/main/java/me/alexthered/graphqlapp/GraphqlappApplication.java
@@ -0,0 +1,13 @@
+package me.alexthered.graphqlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GraphqlappApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/alexthered/graphqlapp/GraphqlappApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alexthered.graphqlapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
